59216557c0880fd134e7ee24902edffa337b4df3
Grigoriy <grinyam@list.ru>
2024-11-05 21:34:07 +0300

    add innerHTML tamplate

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const data = {
-  "sources":[{"id":1,"name":"meduza"},{"id":6,"name":"3dnews"},{"id":7,"name":"nytimes"},{"id":8,"name":"forbes"},{"id":9,"name":"igromania"},{"id":10,"name":"buro237"},{"id":11,"name":"rusvesna"},{"id":12,"name":"7ya"},{"id":14,"name":"aif"},{"id":15,"name":"gazetaru"},{"id":16,"name":"karpov.courses"}],"categories":[{"id":1,"name":"tech"},{"id":2,"name":"sport"},{"id":3,"name":"fashion"},{"id":4,"name":"politics"},{"id":5,"name":"other"},{"id":6,"name":"karpov.courses"}],"items":[{"id":537280,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Таисия Повалий призналась, что экономит после покупки дома в Подмосковье","description":"Недвижимость певица приобрела в ипотеку.","image":"https://aif-s3.aif.ru/images/036/644/1d793dbeae802f8455b1210f9b356d6a.jpg","source_id":14,"category_id":5},{"id":537078,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Сериал по The Last of Us получил награду Гильдии сценаристов США","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/4/15/d5a557ee-c145-40ff-a03a-785b42b14d6f.jpg","source_id":9,"category_id":1},{"id":536983,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Женская сборная Канады выиграла чемпионат мира по хоккею в 13-й раз","description":"Женская сборная Канады выиграла чемпионат мира по хоккею в 13-й раз в истории. В финальном матче канадские хоккеистки обыграли сборную США в овертайме","image":"https://cdnn11.img.sputnik.by/img/07e8/04/0f/1085478388_0:0:3072:1728_2072x0_60_0_0_d7c288d848193e24c7b375b72ed4ba0e.jpg","source_id":8,"category_id":5},{"id":536810,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Россиянин с праздника попал на операционный стол с язвой","description":"Мужчина с праздничного ужина попал на операционный стол с язвой. Об этом сообщает Люберецкая областная больница.\n\nВ Люберецкую больницу поступил 58-летний мужчина с жалобами на острую боль в области живота, возникшую у него сразу после ...","image":"https://img.gazeta.ru/files3/856/14752856/surgeons-performing-operation-operation-room-pic905-895x505-17732.jpg","source_id":15,"category_id":1},{"id":536293,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Актриса Дженнифер Энистон вышла на публику в облегающем топе","description":"Голливудская актриса Дженнифер Энистон посетила ежегодный фестиваль PaleyFest, посвященный телевизионным проектам. Об этом сообщает Daily Mail.\n\nВ пятницу, 12 апреля, в театре \"Долби\" в Лос-Анджелесе прошел первый вечер ...","image":"http://img.gazeta.ru/files3/116/18570116/56-pic905-895x505-90676.jpg","source_id":15,"category_id":3},{"id":536205,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Семь человек пострадали в ДТП в Ярославской области","description":"","image":"","source_id":14,"category_id":5},{"id":536033,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Немецкий политик обрушилась с критикой на Шольца за политику по Украине ","description":"Депутат бундестага Сара Вагенкнехт обрушилась с критикой на канцлера ФРГ Олафа Шольца за неспособность Германии инициировать переговоры по урегулированию украинского конфликта, по ее словам, однопартиец Шольца экс-канцлер Германии Герхард ...","image":"https://img.gazeta.ru/files3/961/18189961/75-pic905-895x505-16828.jpg","source_id":15,"category_id":4},{"id":535653,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Экс-солист «Ласкового мая»: мои близкие остались в затопленном Оренбурге","description":"Артист отметил, что в его детстве такого разлива реки Урал никогда не было.","image":"https://aif-s3.aif.ru/images/036/618/72f337693dd18ab96ba4fcced3caa5cc.jpg","source_id":14,"category_id":5},{"id":535480,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Охлобыстин провел творческую встречу с курсантами вузов Минобороны РФ","description":"Актер рассказал о своем опыте участия в гуманитарных поездках в зону проведения СВО.","image":"https://aif-s3.aif.ru/images/036/604/2743fc01481b45ad80a76ad8e23d61e5.jpg","source_id":14,"category_id":4},{"id":535400,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Уровень реки Урал в Орске опустился ниже критического уровня","description":"Подтопленными остаются 5 182 жилых домов.","image":"https://aif-s3.aif.ru/images/036/607/8402ed2b096e72727731fba3efa705c1.JPG","source_id":14,"category_id":4},{"id":534518,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Electronic Arts повысит стоимость подписки EA Play, чтобы не отставать от конкурентов","description":"Портал GamesIndustry.biz предупредил, что американский издатель и разработчик Electronic Arts вслед за основными конкурентами готовится повысить стоимость своей подписки EA Play.","image":"https://3dnews.ru/assets/external/illustrations/2024/04/10/1103092/00.jpg","source_id":6,"category_id":1},{"id":534031,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Немецкий бизнесмен указал на провал плана НАТО по подавлению России","description":"Немецкий бизнесмен Ким Дотком заявил, что план НАТО по подавлению России провалился, а российская армия на сегодняшний день сильнее, чем когда-либо. Об этом он написал на своей странице в соцсети Х (ранее Twitter).\n\n\"НАТО даже не ...","image":"https://img.gazeta.ru/files3/157/14328157/RIAN_6516266.HR-pic905-895x505-79523.jpg","source_id":15,"category_id":4},{"id":533585,"lang":"ru","date":"Fri Jun 07 2024 03:00:00 GMT+0300 (Moscow Standard Time)","title":"Star Wars Outlaws получила сюжетный трейлер с датой выхода в конце августа","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/4/9/455933e5-307c-48d3-8412-edbd70623666.jpg","source_id":9,"category_id":1},{"id":536636,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"Ведущий сценарист Fallout 4 запутал фанатов заявлением о главном герое-мужчине","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/4/14/6df0d198-204e-49f4-a3b4-0870d1e163b4.jpg","source_id":9,"category_id":1},{"id":536208,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"Обнародована личность мужчины, бросившего «коктейль Молотова» во Владимире","description":"Горючее 24-летний мужчина кинул возле здания областной администрации.","image":"https://aif-s3.aif.ru/images/036/625/8ea4d1bbd4df11e2a58d7f0774353c56.png","source_id":14,"category_id":5},{"id":535831,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"Съемки ленты «Чебурашка 2» начнутся в мае","description":"По словам продюсера фильмов «Чебурашка», съемки второй части серии семейных комедий стартуют в мае. Премьера ленты запланирована на 1 января 2026 года. Над второй частью «Чебурашки» продолжит работать режиссер Дмитрий Дьяченко","image":"","source_id":8,"category_id":5},{"id":535795,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"В Кремле оценили предложение Эстонии о признании МП террористическим","description":"Песков указал, что заявление главы МВД Эстонии о признании Московского патриархата террористическим вряд ли можно отнести к категории самых умных.","image":"https://aif-s3.aif.ru/images/036/613/1e899965419df5430a61f08b21e67afc.jpg","source_id":14,"category_id":5},{"id":535654,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"«Графика просто улёт»: блогер воссоздал трейлер GTA VI в реальной жизни","description":"При создании антуража криминального боевика GTA VI разработчики из Rockstar Games вдохновлялись американским штатом Флорида, но блогер Эндрю Левитт (Andrew Levitt) решил вывести подражание реальности на новый уровень. \n#grandtheftautovi #gtavi #rockstargames #экшен","image":"https://3dnews.ru/assets/external/illustrations/2024/04/12/1103190/00.jpg","source_id":6,"category_id":1},{"id":535642,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"Авторы The Outlast Trials насчитали 2 миллиона игроков и объявили Toxic Shock","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/4/12/7f714e39-bf35-40b5-9c90-c6fc0704fc5c.jpg","source_id":9,"category_id":1},{"id":535488,"lang":"ru","date":"Fri Jun 07 2024 02:00:00 GMT+0300 (Moscow Standard Time)","title":"Родился 12 апреля? Будешь Юрой!","description":"В день первого полета человека в космос 12 июня 1961 года в Москве родилось 262 новорожденных. С 1961 года в День космонавтики родилось около...","image":"https://img.7ya.ru/fresh/2024/4/12/1712910460552.jpg","source_id":12,"category_id":5}]
-}
+let data = null;
 
 const mainNews = data.items.slice(0, 3);
 const smallNews = data.items.slice(3, 12);
@@ -8,32 +6,15 @@ const smallNews = data.items.slice(3, 12);
 const mainNewsContainer = document.querySelector('.articles__big-column');
 const smallNewsContainer = document.querySelector('.articles__small-column');
 
-const createSmallNewsItem = (item) => {
-  const sourseData = data.sources.find((sourseItem) => sourseItem.id === item.source_id);
-  const dateData = new Date(item.date).toLocaleDateString('ru-RU', {month: 'long', day: 'numeric'});
-
-  const article = document.createElement('article');
-  const textContainer = document.createElement('p')
-  const title = document.createElement('h2');
-  const sourse = document.createElement('span')
-  const date = document.createElement('span')
-
-  article.classList.add('small-article')
-  textContainer.classList.add('small-article__caption')
-  title.classList.add('small-article__title')
-  sourse.classList.add('article-sourse', 'small-article__sourse')
-  date.classList.add('article-date', 'small-article__date')
-
-  title.textContent = item.title;
-  sourse.textContent = sourseData.name;
-  date.textContent = dateData;
-
-  textContainer.appendChild(date);
-  textContainer.appendChild(sourse);
-  article.appendChild(title);
-  article.appendChild(textContainer);
-
-  return article;
+const escapeString = (string) => {
+  const symbols = {
+    '&': '&amp',
+    '<': '&lt',
+    '>': '&gt'
+  };
+  return string.replace(/[&<>]/g, (tag) => {
+    return symbols[tag] || tag;
+  })
 }
 
 mainNews.forEach((item) => {
@@ -47,10 +28,10 @@ mainNews.forEach((item) => {
       <img src="${item.image}" alt="Фото новости" class="main-article__image">
     </div>
     <div class="main-article__content">
-      <span class="article-category main-article__category">${categoryData.name}</span>
-      <h2 class="main-article__title">${item.title}</h2>
-      <p class="main-article__text">${item.description}}</p>
-      <span class="article-sourse main-article__sourse">${sourseData.name}</span>
+      <span class="article-category main-article__category">${escapeString(categoryData.name)}</span>
+      <h2 class="main-article__title">${escapeString(item.title)}</h2>
+      <p class="main-article__text">${escapeString(item.description)}}</p>
+      <span class="article-sourse main-article__sourse">${escapeString(sourseData.name)}</span>
     </div>
   </article>
 `
@@ -64,12 +45,20 @@ smallNews.forEach((item) => {
 
   template.innerHTML =`
   <article class="small-article">
-    <h2 class="small-article__title">${item.title}</h2>
+    <h2 class="small-article__title">${escapeString(item.title)}</h2>
     <p class="small-article__caption">
       <span class="article-date small-article__date">${dateData}</span>
-      <span class="small-article-sourse article-sourse">${sourseData.name}</span>
+      <span class="small-article-sourse article-sourse">${escapeString(sourseData.name)}</span>
     </p>
   </article>
   `
   smallNewsContainer.appendChild(template.content)
 })
+
+const renderNews = (categoryId) => {
+  fetch('http://frontend.karpovcourses.net/api/v2/ru/news')
+    .then(response => response.json())
+    .then((responeData) => {
+      data = responeData;
+    })
+}
